c3ab03161a33fd0abd042ab23185f1bf252f0f6d
Santhosh <santhoshmuruganandham023@gmail.com>
2025-11-24 13:50:20 +0530

    Second Commit

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -9,7 +10,8 @@ export default defineConfig({
       },
     }),
   ],
+  base: '/my-portfolio/', // <-- ADD THIS: your GitHub repo name
   build: {
-    outDir: "dist"
-  }
+    outDir: 'dist',
+  },
 })
